557160308ebefb46a342e5da96283b5e7bc074e1
morimoto12345 <guitar.morimoto.894@gmail.com>
2023-02-22 16:24:49 +0900

    initial commit

---
+++ 1stDay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(123);
+            Console.WriteLine(456);
+
+            Console.Write("ABC");
+            Console.WriteLine("DEF");
+
+        }
+    }
+}
